6457eca57599fbc456f153e5592899d871d49d55
Técnico em Informática - 2018.3 <169121742018.3@senacrjedu.local>
2019-10-31 10:45:04 -0300

    transferindo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- chamada do boostrep-->
+
+    <link rel="stylesheet" type="text/css" href="../projeto/bootstrap/css/bootstrap.css">
+
+</head>
+<body>
+	
+	<!-- o código tem que estar dentro do divisão comtainer-->
+
+	<div class="container">
+	<a href="Logar.php">
+		LOGAR NO SISTEMA	
+    </div>
+</body>
+</html>
